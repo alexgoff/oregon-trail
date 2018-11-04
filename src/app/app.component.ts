5082a1450589e142e79667623105c4d7f7ac3abe
Alex Goff <alexander.r.goff@gmail.com>
2018-11-04 10:33:42 -0500

    add figlet and theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as figlet from 'figlet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'oregon-trail';
+  // testString = ascii.font('test');
+
+  ngOnInit () {
+    figlet('Hello World!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      console.log(data)
+  });    
+  }
 }
